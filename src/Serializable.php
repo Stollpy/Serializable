5509c061b9a7901c0f31e87f4b7ebaa9616c5cb3
Stollpy <darrasflorian42@gmail.com>
2024-01-09 17:24:33 -0500

    init commit

---
+++ src/Serializable.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the Laradev project
+ * (c) Darras Florian florian@laradev.ca
+ */
+
+use ArrayAccess;
+
+trait Serializable
+{
+    public function toArray(object|string|null $object = null): array
+    {
+        $data = [];
+        $object = $object ?? $this;
+        $ref = new \ReflectionClass($object);
+
+        foreach ($ref->getProperties() as $property) {
+            if (
+                !in_array($property->getName(), ["whitelist", "blacklist"]) &&
+                $this->checkWhitelist($property, $object) &&
+                !$this->checkBlacklist($property, $object)
+            ) {
+                $value = $this->extractValueFromProperty($property, $object);
+                $data[$property->getName()] = $this->extractValueFromType($value);
+            }
+        }
+
+        return $data;
+    }
+
+    private function checkWhitelist(\ReflectionProperty $property, object $object): bool
+    {
+        $ref = new \ReflectionClass($object);
+        $whitelist = [];
+        $whitelistExist = false;
+
+        if ($ref->hasProperty("whitelist")) {
+            $whitelist = $object->whitelist;
+            $whitelistExist = true;
+        }
+
+        if ($ref->hasMethod("whitelist")) {
+            $whitelist = $this->whitelist();
+            $whitelistExist = true;
+        }
+
+        if (!$whitelistExist) {
+            return true;
+        }
+
+        return in_array($property->getName(), $whitelist);
+    }
+
+    private function checkBlacklist(\ReflectionProperty $property, object $object): bool
+    {
+        $ref = new \ReflectionClass($object);
+        $blacklist = [];
+        $blacklistExist = false;
+
+        if ($ref->hasProperty("blacklist")) {
+            $blacklist = $object->blacklist;
+            $blacklistExist = true;
+        }
+
+        if ($ref->hasMethod("blacklist")) {
+            $blacklist = $this->blacklist();
+            $blacklistExist = true;
+        }
+
+        if (!$blacklistExist) {
+            return false;
+        }
+
+        return in_array($property->getName(), $blacklist);
+    }
+
+    private function extractValueFromProperty(\ReflectionProperty $property, object $object): mixed
+    {
+        if (!$property->isInitialized($object)) {
+            return null;
+        }
+
+        if ($property->isPublic()) {
+            return $property->getValue($object);
+        }
+
+        $camelCaseProperty = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $property->getName()))));
+        $getter = 'get'.ucfirst($camelCaseProperty);
+
+        if ((new \ReflectionClass($object))->hasMethod($getter)) {
+            return $object->$getter();
+        }
+
+        return null;
+    }
+
+    private function extractValueFromType(mixed $attr): mixed
+    {
+        if (is_array($attr) || $attr instanceof ArrayAccess) {
+            $data = [];
+            foreach ($attr as $key => $value) {
+                $data[$key] = $this->extractValueFromType($value);
+            }
+            return $data;
+        } elseif ($attr instanceof \BackedEnum) {
+            return $attr->value;
+        } elseif (is_object($attr)) {
+            return $this->toArray($attr);
+        } else {
+            return $attr;
+        }
+    }
+
+    public function toJson(object|string|null $object = null): string
+    {
+        return json_encode($this->toArray($object ?? $this));
+    }
+}
